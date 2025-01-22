3a4d0063ec0c1b2f4f15178f0a0812c6215dd97c
ricardovac <ricardoviicius@gmail.com>
2025-01-21 23:33:30 -0300

    refactor: structure

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,21 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
-import * as dotenv from 'dotenv'
-import * as fs from 'fs'
 import { defineConfig, UserConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const NODE_ENV = mode || 'development'
-  const envFiles = [`.env.${NODE_ENV}`]
-  for (const file of envFiles) {
-    const envConfig = dotenv.parse(fs.readFileSync(file))
-    for (const k in envConfig) {
-      process.env[k] = envConfig[k]
-    }
-  }
+  // const NODE_ENV = mode || 'development'
+  // const envFiles = [`.env.${NODE_ENV}`]
+  // for (const file of envFiles) {
+  //   const envConfig = dotenv.parse(fs.readFileSync(file))
+  //   for (const k in envConfig) {
+  //     process.env[k] = envConfig[k]
+  //   }
+  // }
 
   const alias = {
     '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -33,15 +31,19 @@ export default defineConfig(({ mode }) => {
       'process.env': {},
     },
     server: {
-      open: true,
-      port: Number(process.env.VITE_WEB_PORT),
+      port: 3000,
+      host: true,
       proxy: {
         '/api': {
-          target: `${process.env.VITE_BASE_PATH}:${process.env.VITE_SERVER_PORT}/`,
+          target: `http://localhost:8080/`,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
+    },
+    preview: {
+      port: 3000,
+      host: true,
     },
     plugins: [vue(), vueDevTools()],
     esbuild,
